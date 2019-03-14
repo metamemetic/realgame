1f9a9e5ead4567969a9984e9950e21721f869293
Christopher David <chris@arcade.city>
2019-03-14 09:36:58 -0500

    And show charge id

--- game/app/Http/Controllers/OpenNodeController.php
+++ game/app/Http/Controllers/OpenNodeController.php
@@ -19,8 +19,8 @@ class OpenNodeController extends Controller
     public function demo_charge()
     {
         $charge_params = array(
-                           'description'       => '1x Book', //Optional
-                           'amount'            => 20.00,
+                           'description'       => '1 Arcade Token', //Optional
+                           'amount'            => 0.25,
                            'currency'          => 'USD', //Optional
                            'order_id'          => 'YOUR-PLATFORM-ID', //Optional
                            'email'             => 'johndoe@example.com', //Optional
@@ -33,9 +33,12 @@ class OpenNodeController extends Controller
         try {
             $charge = \OpenNode\Merchant\Charge::create($charge_params);
 
-            echo 'LN BOLT11: ' . $charge->lightning_invoice['payreq'];
-
+            echo 'Charge id: ' . $charge->id;
             echo '<br /><br />';
+
+            echo 'LN BOLT11: ' . $charge->lightning_invoice['payreq'];
+            echo '<br /><br />';
+
             echo 'BTC address: ' . $charge->chain_invoice['address'];
             echo '<br /><br />';
             dd($charge);
@@ -50,7 +53,7 @@ class OpenNodeController extends Controller
     public function charge_info()
     {
         try {
-            $charge = \OpenNode\Merchant\Charge::find('43109de4-1d72-46ed-9415-6d3c46b18485');
+            $charge = \OpenNode\Merchant\Charge::find('25541bd8-239a-43cb-be88-5f2b5a38dce8');
 
             if ($charge) {
                 dd($charge);
@@ -59,7 +62,7 @@ class OpenNodeController extends Controller
                 echo 'Charge not found';
             }
         } catch (Exception $e) {
-          echo $e->getMessage(); // Unauthorized Not authorized: invalid api key
+            echo $e->getMessage(); // Unauthorized Not authorized: invalid api key
         }
     }
 
