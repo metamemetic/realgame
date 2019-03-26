e261bffabb52d7368c2b0587a744c60debdba60c
Christopher David <chris@arcade.city>
2019-03-26 10:53:09 -0500

    Pull in arca engine object with options

--- game/resources/js/arca/index.js
+++ game/resources/js/arca/index.js
@@ -1,5 +1,22 @@
-console.log('Hello from Arca')
+/**
+ * Arca: Lightweight voxel engine built on BabylonJS, inspired by noa-engine
+ */
 
-module.exports = {
-    hello: 'world'
+var extend = require('extend')  // Extend one object with one or more others, returning the modified object
+var vox = require('vox.js')     // MagicaVoxel file parser
+
+module.exports = Engine
+
+var defaults = {
+    userControlsCamera: true
+}
+
+/**
+ * Main engine object
+*/
+
+function Engine(opts) {
+    if (!(this instanceof Engine)) return new Engine(opts)
+    opts = extend(defaults, opts)
+    console.log('Arca initialized with opts:', opts)
 }
